7c4ff30c3f4ccd2b12d8a1175f86a7ad37e5d310
Ernesto Cuadros-Vargas <ecuadros@gmail.com>
2026-09-05 12:21:37 -0500

    C++23 activated

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 // g++ main.cpp -o main
+// para ejecutar: ./main
+// g++ -std=c++23 main.cpp -o main
 int main() {
     cout << "Hello, World! CS-UNI!" << endl;
     return 0;
